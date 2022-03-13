2ff5b7f54c682c83e36daf3c4bde1e452e9b432d
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-13 17:25:18 +0800

    fill the empty dictionary and print

--- 5forDictionary2.py
+++ 5forDictionary2.py
@@ -1,4 +1,7 @@
 import random
 keys=["strength","dexterity","constitution","intelligence","wisdom","charisma"]
 characterB={}
-
+for i in range(6):
+    characterB[keys[i]]=random.randint(0,20)
+for key,value in characterB.items():
+    print(key,value)
