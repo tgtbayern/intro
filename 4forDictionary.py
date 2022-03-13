ad799990b1ec365ba38cb0bfe695239343198d43
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-13 17:36:30 +0800

    make it beautiful

--- 4forDictionary.py
+++ 4forDictionary.py
@@ -1,4 +1,9 @@
-characterA={"strength":10,"dexterity":15,"constitution":19,"intelligence":20,"wisdom":7,"charisma":5}
+characterA={"strength":10,
+"dexterity":15,
+"constitution":19,
+"intelligence":20,
+"wisdom":7,
+"charisma":5}
 for key,value in characterA.items():
     print(key,value)
 print(characterA)
