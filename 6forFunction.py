a57aeb7699fb69542c9d61da724c1ff96ddd3d2c
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-13 18:15:14 +0800

    main function

--- 6forFunction.py
+++ 6forFunction.py
@@ -1,2 +1,14 @@
+from cgitb import reset
+
+
 def converts(celcius):
-    return celcius*9/5+32
+    result=celcius*9/5+32
+    return result
+if __name__ == "__main__":
+    while(True):
+        message=input("Input the celcius or q for quit")
+        if(message.lower()=='q'):
+            print("quit")
+            break
+        message1=int(message)
+        print("The result is",converts(message1))
