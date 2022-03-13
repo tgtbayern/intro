b85d62caa47ea3311150fe79d3a761fd39bb3b38
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-13 17:35:29 +0800

    something else not required in tutorials

--- 5forDictionary2.py
+++ 5forDictionary2.py
@@ -5,3 +5,23 @@ for i in range(6):
     characterB[keys[i]]=random.randint(0,20)
 for key,value in characterB.items():
     print(key,value)
+#Test something else about dictionary,not required in the tutorials
+
+#access dictionary
+print(characterB["strengeth"])
+#print whole dictionary
+print(characterB)
+#print key and value seperately
+for key,value in characterB.items():
+    print(key,value)
+#add something in dictionary
+for i in range(6):
+    characterB[keys[i]]=random.randint(0,20)
+print(characterB)
+#change the value
+characterB["strengeth"]=99
+print(characterB)
+#delete Key/value pair
+del characterB["strengeth"]
+print(characterB)
+
