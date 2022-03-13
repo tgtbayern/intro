fbd02be55316cb021501ddde37a80baa485f42c0
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-13 16:45:29 +0800

    print key

--- 4forDictionary.py
+++ 4forDictionary.py
@@ -1,4 +1,7 @@
 characterA={"strength":10,"dexterity":15,"constitution":19,"intelligence":20,"wisdom":7,"charisma":5}
 for key,value in characterA.items():
-    print(key)
-    print(value)
+    print(key,value)
+print(characterA)
+for key in characterA.keys():
+    if key=="strength":
+        print(characterA["strength"])
