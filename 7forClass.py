aef06b136af6c7d8d4cc3bc7790bc89948ff9e56
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-16 15:47:34 +0800

    constructor

---
+++ 7forClass.py
@@ -0,0 +1,8 @@
+class Character:
+    def __init__(self,strength, dexterity, constitution, intelligence, wisdom, charisma):
+        self.strength=strength
+        self.dexterity=dexterity
+        self.constitution=constitution
+        self.intelligence=intelligence
+        self.wisdom=wisdom
+        self.charisma=charisma
