d49d18f4156c9a6cc0c4dcfb2ddfa9300addb49a
tgtbayern <74101072+tgtbayern@users.noreply.github.com>
2022-03-16 16:55:44 +0800

    attack

--- 7forClass.py
+++ 7forClass.py
@@ -1,3 +1,6 @@
+from random import random
+
+
 class Character:
     def __init__(self,strength, dexterity, constitution, intelligence, wisdom, charisma):
         self.strength=strength
@@ -6,3 +9,20 @@ class Character:
         self.intelligence=intelligence
         self.wisdom=wisdom
         self.charisma=charisma
+        self.hitpoint=self.constitution*30+50
+
+    def show_stats(self):
+        print("strength",self.strength)
+        print("dexterity",self.dexterity)
+        print("constitution",self.constitution)
+        print("intelligence",self.intelligence)
+        print("wisdom",self.wisdom)
+        print("charisma",self.charisma)
+        
+    def show_hitpoints(self):
+        print("hitpoint",self.hitpoint)
+
+    def attack(self):
+        print(random.randint(1,self.strength))
+
+   
